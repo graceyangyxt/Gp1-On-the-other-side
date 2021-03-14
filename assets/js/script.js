075694673f52a2886856cbecec842ca6b25ed4bf
yondav <yonidavid.abr@gmail.com>
2021-03-14 01:53:04 -0500

    Added landing page and achieved the same functionality with the map marker. Google maps now works and pulls up a satelite image map of the current location when the user selects the map marker icon. On to the search bar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,3 @@
-const navSearch = document.querySelector('#search-bar');
-const currLocation = document.querySelector('.map-marker');
-const submitBtn = document.querySelector('#search-btn');
-const randomBtn = document.querySelector('#randomize');
 const gKey = 'AIzaSyCRp2lbrs-v_gZuyA8HJfvw6Ih4XKXCyI4';
 
 let lat;
@@ -9,52 +5,91 @@ let lon;
 let antLon;
 let antLat;
 
+// nav elements
+const searchCont = document.querySelector('.search-container');
+const navSearch = document.querySelector('#search-bar');
+const currLocation = document.querySelector('.map-marker');
+const submitBtn = document.querySelector('#search-btn');
+const randomBtn = document.querySelector('#randomize');
+
+// landing page elements
+const landingSearch = document.querySelector('#landing-search-bar');
+const landingCurrLoc = document.querySelector('.landing-map-marker');
+const landingSubmitBtn = document.querySelector('#landing-search-btn');
+const landingRandomBtn = document.querySelector('#landing-randomize');
+
+// pages
+const landingPg = document.querySelector('#landing-pg');
+const initLocationPg = document.querySelector('.initial-location-pg');
+
 // event listeners
 
-// current location icon - get current coordinates
+// **nav-bar** current location icon - get current coordinates
 currLocation.addEventListener('click', function () {
   navigator.geolocation.getCurrentPosition(function (position) {
     lat = position.coords.latitude;
     lon = position.coords.longitude;
     console.log(lat, lon);
 
-    // test to see antipode function works
-    getAntipodes();
-    console.log(antLat, antLon);
-    initMap();
+    initMap(lat, lon);
+    // toggles visibility for the second page successfully
+    if (initLocationPg.classList.contains('hide')) {
+      initLocationPg.classList.remove('hide');
+    }
+    if (landingPg.classList.contains('hide') === false) {
+      landingPg.classList.add('hide');
+    }
+  });
+});
+
+// ** landing page** current location icon - get current coordinates
+landingCurrLoc.addEventListener('click', function () {
+  landingPg.classList.add('hide'); // toggles visibility of landing page.
+  // **** we need a loading bar or animation like Robert suggested while geolocation pulls the coordinates
+  navigator.geolocation.getCurrentPosition(function (position) {
+    lat = position.coords.latitude;
+    lon = position.coords.longitude;
+    console.log(lat, lon);
+
+    initMap(lat, lon);
+    if (initLocationPg.classList.contains('hide')) {
+      initLocationPg.classList.remove('hide');
+    }
+    if (searchCont.classList.contains('hide')) {
+      searchCont.classList.remove('hide'); // toggles visibility of navbar
+    }
   });
 });
 
 // functions
 
 // antipodal coordinates
-function getAntipodes() {
-  antLat = lat * -1;
-  antLon = lon + 180;
+function getAntipodes(x, y) {
+  antLat = x * -1;
+  antLon = y + 180;
 }
 
 // fetch api using coordinates
 function reverseGeo() {}
 
-function initMap() {
-  var options = {
-    center: { lat: lat, lng: lon },
-    zoom: 8,
-  };
-
-  map = new google.maps.Map(document.getElementById('map'), {
-    options,
-  });
+function initMap(x, y) {
+  if (x && y) {
+    var z = 1;
+    var interval = setInterval(increment, 1500);
+    function increment() {
+      if (z < 20) {
+        z = (z % 20) + 3;
+        const map = new google.maps.Map(document.getElementById('map'), {
+          center: { lat: x, lng: y },
+          zoom: z,
+          mapTypeId: 'satellite',
+        });
+        map.setTilt(45);
+      }
+    }
+  }
 }
 
-// functions
-// function getCoords() {
-//   navigator.geolocation.getCurrentPosition(function (position) {
-//     let lat = position.coords.latitude;
-//     let lon = position.coords.longitude;
-//     console.log(lat, lon);
-//   });
-// }
 // // fetch for fish data
 // function getFish() {
 //   fetch('https://fishbase.ropensci.org/species?offset=100')
